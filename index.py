b1055c13de00970bd38a30e6c9a9b25ce1d93486
Mohit Bishesh <74617917+MohitBishesh@users.noreply.github.com>
2023-11-20 16:52:45 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 from flask import Flask
-helloworld = Flask(__name__)
-@helloworld.route("/")
-def run():
-    return "{\"message\":\"Hello I am Mohit and this is my flask App.\"}"
-if __name__ == "__main__":
-    helloworld.run(host="0.0.0.0",port=int("3000"),debug=True)
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
