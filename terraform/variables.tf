dbdf27dde0bededd9247b7ca0f730f385b39865a
Mohit Bishesh <74617917+MohitBishesh@users.noreply.github.com>
2023-11-20 13:09:24 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "ec2-ubuntu-ami" {
+ default = "ami-053b0d53c279acc90"
+}
+
+variable "ec2-linux-ami" {
+ default = "ami-041feb57c611358bd"
+}
+
+variable "ec2-macos-ami" {
+ default = "ami-0051c80cce1c6bbcc"
+} 
