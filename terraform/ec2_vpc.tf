f189d09b0b501b1fa678eac129dbdc242bdee2d3
Mohit Bishesh <74617917+MohitBishesh@users.noreply.github.com>
2023-11-24 11:15:30 +0530

    Update ec2_vpc.tf

--- terraform/ec2_vpc.tf
+++ terraform/ec2_vpc.tf
@@ -1,65 +1,79 @@
-resource "aws_key_pair" "mykey" {
- key_name = "terra-keyy"
- public_key = file("/home/ubuntu/.ssh/terra-keyy.pub")
- }
-resource "aws_default_vpc" "default_vpc"{
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
+  tags = {
+    Name = "Custom VPC"
+  }
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.custom_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet1" {
+  vpc_id                  = aws_vpc.custom_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "Private Subnet 1"
+  }
+}
+
+resource "aws_subnet" "private_subnet2" {
+  vpc_id                  = aws_vpc.custom_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1c"
+
+  tags = {
+    Name = "Private Subnet 2"
+  }
+}
+
 
 resource "aws_security_group" "allow_my_ssh2" {
   name        = "allow_my_ssh2"
-  description = "Allow ssh inbound traffic"
+  description = "Allow SSH inbound traffic"
 
-  # using default VPC
-  vpc_id      = aws_default_vpc.default_vpc.id
+  vpc_id = aws_vpc.custom_vpc.id
+
+  # Ingress rule allowing SSH traffic from any source
   ingress {
-    description = "TLS from VPC"
+    description = "SSH from any source"
 
-    # we should allow incoming and outoging
-    # TCP packets
+    # Allow incoming traffic on port 22 (SSH)
     from_port   = 22
     to_port     = 22
+
+    # Allow SSH traffic using the TCP protocol
     protocol    = "tcp"
 
-    # allow all traffic
+    # Allow SSH traffic from any source IP address
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # Egress rule allowing all outbound traffic
+  egress {
+    # Egress rule allowing all outbound traffic (all protocols, all ports)
+    from_port   = 0
+    to_port     = 0
+
+    # Allow all outbound traffic to any destination IP address
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Tags for identifying the security group
   tags = {
     Name = "allow_ssh2"
   }
-}
-
-resource "aws_instance" "manager_instance" {
- key_name = aws_key_pair.mykey.key_name
- ami             = var.ec2-ubuntu-ami
- instance_type   = "t2.micro"
- security_groups = [aws_security_group.allow_my_ssh2.name]
-
- tags= {
-  Name = "Manager-node(with terraform)"
-}
-}
-
-resource "aws_instance" "worker_instance1" {
- key_name = aws_key_pair.mykey.key_name
- ami             = var.ec2-ubuntu-ami
- instance_type   = "t2.micro"
- security_groups = [aws_security_group.allow_my_ssh2.name]
-
- tags= {
-  Name = "Worker-node1 (with terraform)"
-}
-
-}
-
-
-resource "aws_instance" "worker_instance2" {
- key_name = aws_key_pair.mykey.key_name
- ami             = var.ec2-ubuntu-ami
- instance_type   = "t2.micro"
- security_groups = [aws_security_group.allow_my_ssh2.name]
-
- tags= {
-  Name = "Worker-node2 (with terraform)"
-}
 }
